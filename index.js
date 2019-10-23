72aac2e084e9141e2f98b69ac0b02663fa95f26a
CharlesterWide <charlierubioal@gmail.com>
2019-10-23 16:47:22 +0200

    Inicio del bot

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 //El siguiente código es un ejemplo de servidor web escrito en Node.js.
 
+const TelegramBot = require('node-telegram-bot-api');
+const token = '777213469:AAEUKXNqCErAnNBhOnt4zM5YAda3JHJwLoE';
+const Baobab = new TelegramBot(token, { polling: true });
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hola Mundo\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+Baobab.onText(/^\/start/, function (msg) {
+    console.log(msg);
+    var chatId = msg.chat.id;
+    var username = msg.from.username;
+
+    Baobab.sendMessage(chatId, "Hola, " + username + " soy el Profesor Baobab \n Un experto en Pokemon"+
+    "\n Prueba el comand /help para ver lo que soy capaz de hacer");
+});
+
+Baobab.onText(/^\/help/, function(msg){
+
 });
