dfdce37cf61a3d89a48dd57038990e628a97800b
CharlesterWide <charlierubioal@gmail.com>
2019-10-23 18:21:20 +0200

    Petición de pokemon básica

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//El siguiente código es un ejemplo de servidor web escrito en Node.js.
 
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'Heje que cagada';
+const token = 'Not a token';
 const Baobab = new TelegramBot(token, { polling: true });
+
+var P = require('pokedex-promise-v2');
+var Pokedex = new P();
 
 const http = require('http');
 
@@ -30,5 +32,40 @@ Baobab.onText(/^\/start/, function (msg) {
 });
 
 Baobab.onText(/^\/help/, function(msg){
+    console.log(msg);
+    var chatId = msg.chat.id;
 
+    Baobab.sendMessage(chatId, "Ahora mismo estoy trabajando para ofrecerte más servicios"
+    + "\n Comandos disponibles: "
+    +"/Pokemon Nombre del Pokemon para que te de la inforación de un Pokemon");
+});
+
+
+Baobab.onText(/^\/Pokemon/, function (msg) {
+    console.log(msg);
+    var chatId = msg.chat.id;
+    var texto = msg.text.toString().toLocaleLowerCase();
+    texto = texto.split(" ");
+    var pokemon = texto[1];
+
+    var mensaje = "";
+
+    console.log("Pokemon a buscar: " + pokemon);
+
+    Pokedex.getPokemonByName(pokemon) // with Promise
+    .then(function(response) {
+      //console.log(response);
+      mensaje = "Nombre: " + response.forms[0].name +
+      "\n" + "Índice en la Pokedex: " + response.id.toString();
+      
+      Baobab.sendPhoto(chatId,response.sprites.front_default.toString(),{caption: mensaje});
+    })
+    .catch(function(error) {
+      
+      mensaje = "Error al buscar a "+ pokemon+ "\n Nombre mal introducido o pokemon no existente";
+      Baobab.sendMessage(chatId, mensaje );
+      console.log('There was an ERROR: ', error);
+    });
+
+    
 });
